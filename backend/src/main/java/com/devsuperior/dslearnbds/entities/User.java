c4b9765b8f2d2580caa58be663099d463f5ebe7f
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-11-17 22:25:55 -0300

    Validation, Exception Handling.(OK)

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dslearnbds.entities;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -26,7 +25,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements UserDetails, Serializable {
+public class User implements UserDetails {
 	private static final long serialVersionUID = 1L;
 
 	@Id
